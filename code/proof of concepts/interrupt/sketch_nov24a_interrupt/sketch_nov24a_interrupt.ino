398e7c18f1087a20a04237ee63ee28fe20616aba
RobinCopp <146443186+RobinCopp@users.noreply.github.com>
2023-11-24 22:39:55 +0100

    Add files via upload

---
+++ code/proof of concepts/interrupt/sketch_nov24a_interrupt/sketch_nov24a_interrupt.ino
@@ -0,0 +1,36 @@
+const int buttonPin = 4;   // Druknop op pin 4
+const int ledPin = 3;      // LED op pin 3
+
+volatile bool ledState = false;  // Huidige status van de LED
+volatile unsigned long lastDebounceTime = 0;  // Tijd voor debouncing
+unsigned long debounceDelay = 50;  // Debounce-tijd in milliseconden
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);  // Configureer pin 2 als ingang met pull-up weerstand
+  pinMode(ledPin, OUTPUT);           // Configureer pin 3 als uitgang
+
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, RISING);  // Koppel de interrupt-functie aan de opgaande flank van de drukknop
+}
+
+void loop() {
+  // Doe hier andere taken die onafhankelijk zijn van de drukknopstatus
+}
+
+void buttonInterrupt() {
+  // Lees de status van de drukknop
+  int buttonState = digitalRead(buttonPin);
+
+  // Debounce de drukknop met millis
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // Als de knop opgaand is, wissel de status van de LED
+    if (buttonState == HIGH) {
+      ledState = !ledState;
+
+      // Schakel de LED in of uit op basis van de nieuwe status
+      digitalWrite(ledPin, ledState);
+    }
+
+    // Update de tijd van de laatste debouncing
+    lastDebounceTime = millis();
+  }
+}
